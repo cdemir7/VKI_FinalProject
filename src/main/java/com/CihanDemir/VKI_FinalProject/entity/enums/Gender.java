83efadfa97b63921d43f4178d25811ddb65f33c1
Cihan DEMİR <56312037+cdemir7@users.noreply.github.com>
2023-11-04 20:32:08 +0300

    Update Gender

--- src/main/java/com/CihanDemir/VKI_FinalProject/entity/enums/Gender.java
+++ src/main/java/com/CihanDemir/VKI_FinalProject/entity/enums/Gender.java
@@ -2,6 +2,7 @@ package com.CihanDemir.VKI_FinalProject.entity.enums;
 
 // Bu yapi ile kullanıcıdan veri alırken ve veritabanına veri gönderimi yapılırken
 // 0 veya 1 yerine String olarak veri gönderim ve alım işlemlerinin oluşturulmasını sağlar.
+// Ayrıca kullanıcınin girdilerini kısıtlayarak oluşacak yanlışlarının önüne geçer.
 public enum Gender {
     MALE,
 
