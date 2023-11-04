c40cc5c0defbafacaafd7d03a76c5eb116740e84
Cihan DEMİR <56312037+cdemir7@users.noreply.github.com>
2023-11-04 19:41:32 +0300

    Creating VKI_FinalProject

---
+++ src/main/java/com/CihanDemir/VKI_FinalProject/VkiFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.CihanDemir.VKI_FinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkiFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkiFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CihanDemir/VKI_FinalProject/VkiFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CihanDemir.VKI_FinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkiFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
