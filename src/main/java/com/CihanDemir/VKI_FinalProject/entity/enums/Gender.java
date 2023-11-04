351182a5cd385b415420cfd7fbbbb53694d4e8ec
Cihan DEMİR <56312037+cdemir7@users.noreply.github.com>
2023-11-04 20:29:44 +0300

    Create Gender

---
+++ src/main/java/com/CihanDemir/VKI_FinalProject/entity/enums/Gender.java
@@ -0,0 +1,9 @@
+package com.CihanDemir.VKI_FinalProject.entity.enums;
+
+// Bu yapi ile kullanıcıdan veri alırken ve veritabanına veri gönderimi yapılırken
+// 0 veya 1 yerine String olarak veri gönderim ve alım işlemlerinin oluşturulmasını sağlar.
+public enum Gender {
+    MALE,
+
+    FEMALE,
+}
