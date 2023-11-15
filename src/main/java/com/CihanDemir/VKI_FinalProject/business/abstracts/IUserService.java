d2b8d9c1c824e4e3b8b5a660946d3f15cafed857
Cihan DEMİR <56312037+cdemir7@users.noreply.github.com>
2023-11-15 16:58:34 +0300

    Updated

--- src/main/java/com/CihanDemir/VKI_FinalProject/business/abstracts/IUserService.java
+++ src/main/java/com/CihanDemir/VKI_FinalProject/business/abstracts/IUserService.java
@@ -5,19 +5,14 @@ import com.CihanDemir.VKI_FinalProject.business.dto.requests.update.UpdateUserRe
 import com.CihanDemir.VKI_FinalProject.business.dto.responses.create.CalculateVkiResponse;
 import com.CihanDemir.VKI_FinalProject.business.dto.responses.get.GetAllUsersResponse;
 import com.CihanDemir.VKI_FinalProject.business.dto.responses.update.UpdateUserResponse;
-import com.CihanDemir.VKI_FinalProject.entity.User;
-
 import java.util.List;
 import java.util.UUID;
 
 // Kullanıcı ile veritabanı arasındaki ara katmandır.
 public interface IUserService {
     CalculateVkiResponse add(CalculateVkiRequest calculateVkiRequest);
-
     List<GetAllUsersResponse> getAll();
-
     UpdateUserResponse update(UUID id,UpdateUserRequest updateUserRequest);
-
     void delete(UUID id);
 }
 
--- src/main/java/com/CihanDemir/VKI_FinalProject/business/dto/requests/update/UpdateUserRequest.java
+++ src/main/java/com/CihanDemir/VKI_FinalProject/business/dto/requests/update/UpdateUserRequest.java
@@ -3,15 +3,10 @@ package com.CihanDemir.VKI_FinalProject.business.dto.requests.update;
 import com.CihanDemir.VKI_FinalProject.entity.enums.Gender;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.UUID;
 
 @Getter
 @Setter
